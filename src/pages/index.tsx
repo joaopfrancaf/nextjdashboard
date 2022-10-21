15f214196074fa0a1d28134901c91c2db1ebc50d
joão.frança <joaofrancaf@hotmail.com>
2022-10-21 14:50:30 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1>dash</h1>
+  )
+}
