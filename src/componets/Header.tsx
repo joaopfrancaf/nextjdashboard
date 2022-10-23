1d9a97b1ee05e81e47022e1efa8372e19d27fdac
João Pedro França Freitas <joaopfrancaf@gmail.com>
2022-10-23 13:35:42 -0300

    more

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon} from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar} from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddFill } from 'react-icons/ri'
 export function Header() {
     return (
         <Flex as='header' w="100%" maxWidth={1280} h="20" mx="auto" mt="4" px="6" align="center">
@@ -23,6 +23,23 @@ export function Header() {
                 <Icon as={RiSearchLine} fontSize="20"/> {/*toda vez que for usar uma icone exterior do chakra, tem que passar ela desse jeito*/}
             </Flex>
 
+            <Flex
+                align='CENTER'
+                ml="auto"
+            >
+                <HStack spacing='8' mx='8' pr="8" py="1" color="gray.300" borderRightWidth={1} borderColor="gray.700"> {/*horizontal Stack (o padrao fica em vertical)*/}
+                    <Icon as={RiNotificationLine} fontSize="20"/>
+                    <Icon as={RiUserAddFill} fontSize="20"/>
+                </HStack>
+
+                <Flex align='center'>
+                    <Box mr='4' textAlign="right">
+                        <Text>João Pedro</Text>
+                        <Text color="gray.300">joaofrancaf@hotmail.com</Text>
+                    </Box>
+                <Avatar size="md" name="João Pedro" src=""></Avatar>
+                </Flex>
+            </Flex>
         </Flex>
 
     )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../componets/Header";
+import { Sidebar } from "../componets/Sidebar";
 
 export default function Dashboard () {
     return (
-        <Header/>
+        <Flex direction="column" h="100vh">
+            <Header/>
+
+            <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar/>
+            </Flex>
+        </Flex>
     ) 
 }
