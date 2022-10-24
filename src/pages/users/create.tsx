b66ca3871e38439d87784c8fd739f61cc2108a58
joão.frança <joaofrancaf@hotmail.com>
2022-10-24 11:39:40 -0300

    another changes

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,44 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr,Text, Divider, VStack, SimpleGrid, HStack } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Input } from "../../componets/form/input";
+import { Header } from "../../componets/Header";
+import { Pagination } from "../../componets/pagination";
+import { Sidebar } from "../../componets/Sidebar";
+
+export default function CreateUser() {
+    return (
+        <Box>
+            <Header/>
+
+                <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                    <Sidebar/>
+
+
+                    <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+                        <Heading size="lg" fontWeight="normal" >Criar Usuario</Heading>
+
+                        <Divider my="6" borderColor="gray.700"/>
+
+                        <VStack spacing="8">
+                            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                            <Input name="name" label="nome completo"></Input>
+                            <Input name="email" type="email" label="E-mail"></Input>
+                            </SimpleGrid>
+
+                            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                            <Input name="password" type="password" label="senha"></Input>
+                            <Input name="password_congirmation" type="password" label="confirmar da senha"></Input>
+                            </SimpleGrid>
+                        </VStack>
+
+                        <Flex mt="8" justify="flex-end">
+                            <HStack spacing="4">
+                                <Button colorScheme="whiteAlpha">Cancelar</Button>
+                                <Button colorScheme="pink">Salvar</Button>
+                            </HStack>
+                        </Flex>
+                    </Box>
+                </Flex>
+        </Box>
+    )
+}
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr,Text } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../componets/Header";
+import { Pagination } from "../../componets/pagination";
+import { Sidebar } from "../../componets/Sidebar";
+
+export default function Userlist() {
+    return (
+        <Box>
+            <Header/>
+
+                <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                    <Sidebar/>
+
+
+                    <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+                        <Flex mb="8" justify="space-between" align="center">
+                            <Heading size="lg" fontWeight="normal">Usuarios</Heading>
+                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine}></Icon> }> add user</Button>
+                        </Flex>
+
+                        <Table colorScheme="whiteAlpha">
+                            <Thead>
+                                <Tr>
+                                    <Th px="6" color="gray.300" width="8">
+                                        <Checkbox colorScheme="pink"></Checkbox>
+                                    </Th>
+                                    <Th>Usuarios</Th>
+                                    <Th>data de cadastro</Th>
+                                    <Th width="8"></Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                <Tr>
+                                    <Td px="6">
+                                        <Checkbox colorScheme="pink"></Checkbox>
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight="bold">João Pedro França Freitas</Text>
+                                            <Text fontSize="sm" color="gray.300">joaofrancaf@hotmail.com</Text>
+                                        </Box>
+                                    </Td>
+                                    <Td>12 de abril, 2022</Td>
+                                    <Td><Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiPencilLine}></Icon> }> edit</Button></Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+
+                        <Pagination/>
+                    </Box>
+                </Flex>
+        </Box>
+    )
+}
